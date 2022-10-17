5418924d6c49182eec38728fa1a355ee27841409
Melodie STRZEMPEK <strmel@yahoo.fr>
2022-10-17 16:39:35 +0200

    API ending, add Controller with all routes, a Service and a test

--- api-project/src/main/java/com/openclassrooms/apiproject/repository/EmployeeRepository.java
+++ api-project/src/main/java/com/openclassrooms/apiproject/repository/EmployeeRepository.java
@@ -4,6 +4,6 @@ import com.openclassrooms.apiproject.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // indique que la clas est un bean et son role est de communiquer avec avec une source donnée
+@Repository // indique que la class est un bean et son role est de communiquer avec avec une source donnée
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 }
