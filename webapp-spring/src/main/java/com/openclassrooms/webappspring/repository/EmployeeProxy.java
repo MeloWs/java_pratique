1b04428d4ee72909c175337d6945a398ab758c03
Melodie STRZEMPEK <strmel@yahoo.fr>
2022-10-19 16:16:42 +0200

    End of project appWeb, add controller, service, and a test

--- webapp-spring/src/main/java/com/openclassrooms/webappspring/repository/EmployeeProxy.java
+++ webapp-spring/src/main/java/com/openclassrooms/webappspring/repository/EmployeeProxy.java
@@ -20,8 +20,8 @@ public class EmployeeProxy {
      * Get all employees
      * @return An iterable of all employees
      */
-
     public Iterable<Employee> getEmployees() {
+
         String baseApiUrl = props.getApiUrl();
         String getEmployeesUrl = baseApiUrl + "/employees";
 
@@ -33,10 +33,38 @@ public class EmployeeProxy {
                 new ParameterizedTypeReference<Iterable<Employee>>() {}
         );
 
-        log.debug("Get Employees call " + response.getStatusCode().toString());
+        //log.debug("Get Employees call " + response.getStatusCode().toString());
 
         return response.getBody();
     }
+
+    /**
+     * Get an employee by the id
+     * @param id The id of the employee
+     * @return The employee which matches the id
+     */
+    public Employee getEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String getEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                getEmployeeUrl,
+                HttpMethod.GET,
+                null,
+                Employee.class
+        );
+
+        //log.debug("Get Employee call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Add a new employee
+     * @param e A new employee (without an id)
+     * @return The employee full filled (with an id)
+     */
     public Employee createEmployee(Employee e) {
         String baseApiUrl = props.getApiUrl();
         String createEmployeeUrl = baseApiUrl + "/employee";
@@ -49,8 +77,48 @@ public class EmployeeProxy {
                 request,
                 Employee.class);
 
-        log.debug("Create Employee call " + response.getStatusCode().toString());
+        //log.debug("Create Employee call " + response.getStatusCode().toString());
 
         return response.getBody();
+    }
+
+    /**
+     * Update an employee - using the PUT HTTP Method.
+     * @param e Existing employee to update
+     */
+    public Employee updateEmployee(Employee e) {
+        String baseApiUrl = props.getApiUrl();
+        String updateEmployeeUrl = baseApiUrl + "/employee/" + e.getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Employee> request = new HttpEntity<Employee>(e);
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                updateEmployeeUrl,
+                HttpMethod.PUT,
+                request,
+                Employee.class);
+
+        //log.debug("Update Employee call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Delete an employee using exchange method of RestTemplate
+     * instead of delete method in order to log the response status code.
+     * @param : The employee to delete
+     */
+    public void deleteEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String deleteEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Void> response = restTemplate.exchange(
+                deleteEmployeeUrl,
+                HttpMethod.DELETE,
+                null,
+                Void.class);
+
+        //log.debug("Delete Employee call " + response.getStatusCode().toString());
     }
 }
